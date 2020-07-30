f8e7be5ea4f1922154f869ad6c1abab9c73bf998
georgeys <1342479179@qq.com>
2020-07-30 16:12:53 +0800

    邮箱认证

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    /**
+     * MustVerifyEmail四个方法：
+    hasVerifiedEmail() 检测用户 Email 是否已认证；
+    markEmailAsVerified() 将用户标示为已认证；
+    sendEmailVerificationNotification() 发送 Email 认证的消息通知，触发邮件的发送；
+    getEmailForVerification() 获取发送邮件地址，提供这个接口允许你自定义邮箱字段。
+     */
+    use Notifiable,MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
