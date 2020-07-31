09f3029f6e34134454a062cf76dacf139607bf33
georgeys <1342479179@qq.com>
2020-07-31 13:21:03 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,12 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    //在这个控制器中除了show方法 其它的都经过auth中间件（只有详情页面没有登录可以访问）
+    public function __construct()
+    {
+        $this->middleware('auth',['except' => ['show']]);
+    }
     //
     public function show(User $user)
     {
@@ -18,6 +24,8 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        //只可以访问当前登录账户的编辑页面
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
@@ -26,7 +34,7 @@ class UsersController extends Controller
         $data = $request->all();
         if ($request->avatar)
         {
-            $result = $uploadHandler->save($request->avatar,'avatar',$user->id);
+            $result = $uploadHandler->save($request->avatar,'avatar',$user->id,416);
             if ($result)
             {
                 $data['avatar'] = $result['path'];
