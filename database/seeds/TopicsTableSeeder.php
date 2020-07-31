08da1b917564cd9928ee5b62d912dcf957ab7cee
georgeys <1342479179@qq.com>
2020-07-31 13:54:49 +0800

    分类模型和数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
+        // 去除所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
         $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
             if ($index == 0) {
                 // $topic->field = 'value';
