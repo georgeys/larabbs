56697e187d4f62527981daf1ee55644cfae13472
georgeys <1342479179@qq.com>
2020-07-31 11:12:16 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 
 class UsersController extends Controller
@@ -20,10 +21,18 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploadHandler)
     {
-
-        $user->update($request->all());
+        $data = $request->all();
+        if ($request->avatar)
+        {
+            $result = $uploadHandler->save($request->avatar,'avatar',$user->id);
+            if ($result)
+            {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
 }
