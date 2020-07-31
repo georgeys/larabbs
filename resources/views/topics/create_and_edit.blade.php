02e893bf995b59a27033893d771d46293c6e9fc9
georgeys <1342479179@qq.com>
2020-07-31 17:15:35 +0800

    用户可以编辑话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -39,9 +39,10 @@
 
                                     <div class="form-group">
                                         <select class="form-control" name="category_id" required>
-                                            <option value="" hidden disabled selected>请选择分类</option>
+{{--                                            //没有主题为默认选中--}}
+                                            <option value="" hidden disabled {{$topic->id ? '' :'selected'}}>请选择分类</option>
                                             @foreach ($categories as $value)
-                                                <option value="{{ $value->id }}">{{ $value->name }}</option>
+                                                <option value="{{ $value->id }}" {{$topic->category_id ==$value->id ? 'selected' : ''}}>{{ $value->name }}</option>
                                             @endforeach
                                         </select>
                                     </div>
