cfcc37dd9feb036a64c3f0c78d3cae017147d035
georgeys <1342479179@qq.com>
2020-08-13 10:45:30 +0800

    修复User模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -89,10 +89,10 @@ class User extends Authenticatable implements MustVerifyEmail
         // 如果值的长度等于 60，即认为是已经做过加密的情况
         if (strlen($value) != 60){
         // 不等于 60，做密码加密处理
-            $this->password  = bcrypt($value);
+            $value = bcrypt($value);
         }
 
-        $this->attributes['password'] = bcrypt($value);
+        $this->attributes['password'] = $value;
     }
 
     public function setAvatarAttribute($path)
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -45,6 +45,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'yuan';
         $user->email = '1342479179@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->password = bcrypt('111111');
         $user->assignRole('Founder');
         $user->save();
 
