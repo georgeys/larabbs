e7f99f35cc18656f16d1857fb8b7dc41f0e73594
georgeys <1342479179@qq.com>
2020-08-13 10:12:53 +0800

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,9 +9,17 @@ class PagesController extends Controller
     //
     public function root()
     {
-//
 //        echo ini_get('memory_limit').PHP_EOL;
-
         return view('pages.root');
+    }
+
+    public function permissionDenied()
+    {
+        // 如果当前用户有权限访问后台，直接跳转访问
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+        // 否则使用视图
+        return view('pages.permission_denied');
     }
 }
